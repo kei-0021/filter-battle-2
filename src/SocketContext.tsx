0c0518a631b5a86fbecc90808846416b79c85abe
kei-0021 <quita02@gmail.com>
2025-07-27 04:31:50 +0000

    issue #2 socket接続にオプションを追加しました

--- src/SocketContext.tsx
+++ src/SocketContext.tsx
@@ -4,7 +4,9 @@ import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
 
-const socket: Socket = io(SOCKET_URL);
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+});
 
 const SocketContext = createContext<Socket>(socket);
 
